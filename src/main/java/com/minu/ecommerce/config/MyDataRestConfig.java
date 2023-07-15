57bc58aa4d90e290a3a6c163185f96d04fcb2fbd
Minwoo <made_power@naver.com>
2023-07-15 15:50:04 +0900

    Entity의 Rest API 호출 시 ID 노출 되도록 변경
    ID 값을 이용하기 위해 기존에 보이지 않았던 ID 값을 노출하도록 변경하였다.

--- src/main/java/com/minu/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/minu/ecommerce/config/MyDataRestConfig.java
@@ -1,14 +1,30 @@
 package com.minu.ecommerce.config;
 
 import com.minu.ecommerce.domain.Product;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public MyDataRestConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
         HttpMethod[] theUnsupportedActions = {HttpMethod.PUT,HttpMethod.POST,HttpMethod.DELETE};
@@ -25,5 +41,24 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 .withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
                 .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
 
+        exposeIds(config);
     }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+
+        // - get a list of all entity classes from the entity manager
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        List<Class> entityClasses = new ArrayList<>();
+
+        // -get entity types for the entities
+        for (EntityType tempEntityType : entities){
+            entityClasses.add(tempEntityType.getJavaType());
+        }
+
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
+    }
+
+
 }
