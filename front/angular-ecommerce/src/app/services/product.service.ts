2eb1073f54c864ff2b0e6f6b4cc1f628233459bc
Minwoo <made_power@naver.com>
2023-07-16 16:56:25 +0900

    상세 페이지 구현
    상세 페이지 컴포넌트를 생성한 후 REST API 와 연결하여 화면에 표시하도록 구현

--- front/angular-ecommerce/src/app/services/product.service.ts
+++ front/angular-ecommerce/src/app/services/product.service.ts
@@ -9,7 +9,8 @@ import { ProductCategory } from '../common/product-category';
   providedIn: 'root'
 })
 export class ProductService {
-  
+
+ 
   private baseUrl = 'http://localhost:8080/api/products';
 
   private categoryUrl = 'http://localhost:8080/api/product-category';
@@ -22,6 +23,12 @@ export class ProductService {
     );
   }
 
+  getProduct(theProductId: number): Observable<Product> {
+    const productUrl = `${this.baseUrl}/${theProductId}`;
+
+    return this.httpClient.get<Product>(productUrl);
+  }
+ 
   getProductList(theCategoryId: number): Observable<Product[]>{
         const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
