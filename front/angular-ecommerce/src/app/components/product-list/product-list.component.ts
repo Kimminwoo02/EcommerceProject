41902be724db2087f5791fccbc5d5992bc570feb
Minwoo <made_power@naver.com>
2023-07-16 19:14:49 +0900

    페이지네이션 구현
    페이지네이션을 구현했다. 스프링은 0부터 시작하고 Angular는 1부터 시작하기 때문에 초기값을 맞추는 로직을 구현했다.

--- front/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ front/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -10,10 +10,17 @@ import { ProductService } from 'src/app/services/product.service';
 })
 export class ProductListComponent implements OnInit {
 
-  products!: Product[];
-  currentCategoryId!: number;
+  products: Product[] = [];
+  currentCategoryId: number = 1;
+  previousCategoryId: number =1;
   searchMode: boolean = false;
 
+  // new properties for pagination
+
+  thePageNumber: number = 1;
+  thePageSize: number = 5;
+  theTotalElements: number = 0;
+  
   constructor(private productService: ProductService,
               private route: ActivatedRoute) { }
 
@@ -58,12 +65,27 @@ export class ProductListComponent implements OnInit {
       this.currentCategoryId =1;
     }
 
-    // now get the products for the given category id
-    this.productService.getProductList(this.currentCategoryId).subscribe(
-      data => {
-        this.products = data;
-      }
-    )
+    if(this.previousCategoryId != this.currentCategoryId){
+      this.thePageNumber = 1;
+    }
 
+    this.previousCategoryId = this.currentCategoryId;
+
+    console.log(`currentCategoryId=${this.currentCategoryId}, thenPageNumber=${this.thePageNumber}`);
+
+
+    this.productService.getProductListPaginate(this.thePageNumber -1,
+                                              this.thePageSize,
+                                              this.currentCategoryId)
+                                              .subscribe(
+                                                data=>{
+                                                  this.products = data._embedded.products;
+                                                  this.thePageNumber = data.page.number + 1;
+                                                  this.thePageSize = data.page.size;
+                                                  this.theTotalElements = data.page.totalElements;
+                                                }
+                                              );
+
+    
   }
 }
