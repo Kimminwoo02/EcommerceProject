8d6fbcc03d908f07aec7c5c1eeed7fc85a5ee14c
Minwoo <made_power@naver.com>
2023-07-22 01:55:56 +0900

    Okta 의존성 추가
    로그인 뷰와 로직을 위해 Okta 의존성을 추가

---
+++ front/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+    oidc: {
+        clientId: '0oaah3lmr4rxeV0Ek5d7',
+        issuer: 'https://dev-88361851.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
